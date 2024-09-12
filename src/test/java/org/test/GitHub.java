a169dacb5984c6c60196f28f4018b8fd54d2332a
Bala200322 <bsubramani855@gmail.com>
2024-09-12 10:34:01 +0530

    Gitproject

---
+++ src/test/java/org/test/GitHub.java
@@ -0,0 +1,29 @@
+package org.test;
+
+public class GitHub {
+	public static void main(String[] args) {
+		
+	System.out.println("pradeep");	
+	System.out.println("bala");	
+	System.out.println("raghul");
+	System.out.println("gowtham");
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
